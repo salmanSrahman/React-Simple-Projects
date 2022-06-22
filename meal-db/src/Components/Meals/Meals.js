56aad4fa9aaff72ce5c021b621d4bdbc15cd6bb9
Salman-S-Rahman <salmanmern@gmail.com>
2022-06-22 14:55:57 +0600

    meal db api practice

---
+++ meal-db/src/Components/Meals/Meals.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./Meals.module.css";
+
+const Meals = () => {
+  return (
+    <div className={style.Meals__container}>
+      <h1>Meals</h1>
+    </div>
+  );
+};
+
+export default Meals;
